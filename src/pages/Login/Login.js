a26cba6513a1a6312edbd9dc1d47f20eae03309d
Abhishek S <abhisheksunil92@gmail.com>
2022-05-18 00:26:56 +0530

    initialized firebase auth

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export const Login = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
   return <div>Login</div>;
 };
